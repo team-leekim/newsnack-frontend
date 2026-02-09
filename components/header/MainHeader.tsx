eadf9fd1f7d29a5f9b2396b7e3f22c5d7563706d
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2026-02-09 14:37:30 +0900

    fix: 로고 링크 범위 수정

--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 
 const MainHeader = () => {
   return (
-    <Link href="/">
-      <header className="mx-auto flex w-[390px] cursor-pointer items-end gap-2 p-4">
+    <header className="mx-auto flex w-[390px] cursor-pointer items-end gap-2 p-4">
+      <Link href="/">
         <Image src="/logo-navy.png" height={32} width={152} alt="logo" />
-        <p className="typo-body-4-r text-navy-400 bottom-0">세상을 만나는 가벼운 시작</p>
-      </header>
-    </Link>
+      </Link>
+      <p className="typo-body-4-r text-navy-400 bottom-0">세상을 만나는 가벼운 시작</p>
+    </header>
   );
 };
 
